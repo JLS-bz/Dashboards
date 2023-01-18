aabf2d09c6c193fc4e235b7bbf0341be1b1684f0
JLS-bz <60501166+JLS-bz@users.noreply.github.com>
2023-01-17 22:19:17 -0600

    deprecated

--- DPDR subreddit.py
+++ DPDR subreddit.py
@@ -11,13 +11,14 @@ end_epoch=int(dt.datetime(2022, 3, 27).timestamp())
 
 df = pd.DataFrame(api.search_submissions(after=start_epoch, before=end_epoch,
                             filter=['author','title','selftext'],
-                            subreddit='dpdr'))
+                            subreddit='depression_help'))
                             
 df.shape
 df.columns
 del df['d_']
 del df['created']
 
-os.makedirs('/dataset', exist_ok=True)
-df.to_csv('dataset/dpdr.csv')
+os.makedirs('/Datasets', exist_ok=True)
+df.to_csv('Datasets/depression_help.csv')
+
 
